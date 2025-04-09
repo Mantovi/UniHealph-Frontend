af41ea7d186324968439badcab22a20512946b80
Mantovi <leandromandrade01@gmail.com>
2025-04-09 18:52:38 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <h2 className='text-3xl text-blue-800 text-center'>UniHealph</h2>
+      <Button variant="outline" className="justify-center">Button</Button>
+    </>
+  )
+}
+
+export default App
